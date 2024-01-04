571c78a8e089e7b600f36b0b877a5eb35db1a326
Amine-Dz <112625154+DanaAmine@users.noreply.github.com>
2024-01-04 17:45:55 +0100

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,20 @@
+const Orders = [
+    {
+        productName: 'Amine Brd',
+        productNumber: '200',
+        paymentStatus: 'done',
+        status: 'active'
+    },
+    {
+        productName: 'Dana Amine',
+        productNumber: '400',
+        paymentStatus: 'pending',
+        status: 'inActive'
+    },
+    {
+        productName: ' Mohamed Amine',
+        productNumber: '100',
+        paymentStatus: 'done',
+        status: 'Pending'
+    },
+]
